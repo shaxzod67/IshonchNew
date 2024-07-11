7b7bc437032470058403db42d3e2187fac6a2af9
shaxzod67 <uzbuz741@gmail.com>
2024-07-11 15:51:57 +0500

    natijada ozgarish

--- src/companents/Natija/natija.jsx
+++ src/companents/Natija/natija.jsx
@@ -1,8 +1,13 @@
+import React, { useState, useEffect } from 'react';
 import { FaChevronLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
+import { useRef } from 'react';
+import { MdOutlineCancel } from "react-icons/md";
+import './natija.css';
 
 export const Natija = () => {
-  const items = [
+  const allItems = [
     { title: "Kimyo", group: "Guruh LLA 1", link: "/guruhLLA" },
     { title: "Biologiya", group: "Guruh LLA 2", link: "/guruhLLA" },
     { title: "Tarix", group: "Guruh LLA 3", link: "/guruhLLA" },
@@ -12,19 +17,84 @@ export const Natija = () => {
     { title: "Tarix", group: "Guruh LLA 7", link: "/guruhLLA" },
     { title: "Matematika", group: "Guruh LLA 8", link: "/guruhLLA" },
   ];
+  const navRef = useRef();
+  const showNavbar = () => {
+  navRef.current.classList.toggle('navBar')
+  }
+
+  const [filter, setFilter] = useState('Hammasi');
+  const [filteredItems, setFilteredItems] = useState(allItems);
+
+  useEffect(() => {
+    if (filter === 'Hammasi') {
+      setFilteredItems(allItems);
+    } else {
+      setFilteredItems(allItems.filter(item => item.title === filter));
+    }
+  }, [filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.id);
+  };
 
   return (
     <div className="natija">
-      <button className="knopka">
-        <Link to="/">
-          <FaChevronLeft />
-        </Link>
-      </button>
+      <header>
+        <nav className="navv">
+          <button className="knopka">
+            <Link to="/">
+              <FaChevronLeft />
+            </Link>
+          </button>
+          <button className="menu " onClick={showNavbar}>
+            <IoMenu />
+          </button>
+          {/* <button className='menu' onClick={showNavbar}>
+            <MdOutlineCancel />
+          </button> */}
 
-      <section className="text-gray-400 bg-gray-900 body-font">
+          <div className="nav">
+            <h1>Ishonch</h1>
+            <ul>
+              <li>
+                <Link to='/'>Asosiy</Link>
+              </li>
+              <li>
+                <Link to='/'>Kurslar</Link>
+              </li>
+              <li>
+                <Link to='/royhat'>Kursga yozilish</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <aside ref={navRef}>
+        <div className="category">
+          <ul>
+            <li>
+              <input type="radio" name="fan" id="Hammasi" onChange={handleFilterChange} /><label htmlFor="Hammasi">Hammasi</label>
+            </li>
+            <li>
+              <input type="radio" name="fan" id="Kimyo" onChange={handleFilterChange} /><label htmlFor="Kimyo">Kimyo</label>
+            </li>
+            <li>
+              <input type="radio" name="fan" id="Biologiya" onChange={handleFilterChange} /><label htmlFor="Biologiya">Biologiya</label>
+            </li>
+            <li>
+              <input type="radio" name="fan" id="Matematika" onChange={handleFilterChange} /><label htmlFor="Matematika">Matematika</label>
+            </li>
+            <li>
+              <input type="radio" name="fan" id="Tarix" onChange={handleFilterChange} /><label htmlFor="Tarix">Tarix</label>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <section className="section">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {items.map((item, index) => (
+            {filteredItems.map((item, index) => (
               <div key={index} className="lg:w-1/4 md:w-1/2 p-4 w-full">
                 <a className="block relative h-48 rounded overflow-hidden">
                   <img
