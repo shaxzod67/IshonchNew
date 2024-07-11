303d0505f73e149e90d172e79447ed05170e7050
shaxzod67 <uzbuz741@gmail.com>
2024-07-11 16:02:20 +0500

    ozgarish

--- src/companents/Header/header.jsx
+++ src/companents/Header/header.jsx
@@ -81,6 +81,27 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
+                <div className="container px-5 py-24 mx-auto ">
+                    <div className="flex flex-wrap -m-4 text-center">
+                        <div className="p-4 sm:w-1/4 w-1/2">
+                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">2.7K</h2>
+                            <p className="leading-relaxed">O'quvchilar</p>
+                        </div>
+                        <div className="p-4 sm:w-1/4 w-1/2">
+                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">45</h2>
+                            <p className="leading-relaxed">Ustozlar</p>
+                        </div>
+                        <div className="p-4 sm:w-1/4 w-1/2">
+                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">4</h2>
+                            <p className="leading-relaxed">Fanlar</p>
+                        </div>
+                        <div className="p-4 sm:w-1/4 w-1/2">
+                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">2</h2>
+                            <p className="leading-relaxed">Filial</p>
+                        </div>
+
+                    </div>
+                </div>
                 <div className="container px-5 py-24 mx-auto">
                     <div className="text-center mb-20">
                         <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">Bizning o'quv markazimizdagi fanlar </h1>
@@ -250,27 +271,7 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-wrap -m-4 text-center">
-                        <div className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">2.7K</h2>
-                            <p className="leading-relaxed">O'quvchilar</p>
-                        </div>
-                        <div className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">45</h2>
-                            <p className="leading-relaxed">Ustozlar</p>
-                        </div>
-                        <div className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">4</h2>
-                            <p className="leading-relaxed">Fanlar</p>
-                        </div>
-                        <div className="p-4 sm:w-1/4 w-1/2">
-                            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">2</h2>
-                            <p className="leading-relaxed">Filial</p>
-                        </div>
-
-                    </div>
-                </div>
+               
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-col text-center w-full mb-20">
                         <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">OUR TEAM</h1>
